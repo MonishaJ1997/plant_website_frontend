7a405e73f83ede5b74d41aa12c1771adcb770982
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-18 12:04:30 +0530

    Update BlogDetails.jsx

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -12,7 +12,7 @@ function BlogDetails() {
   const navigate = useNavigate();
   const location = useLocation();
 
-  const BASE_URL = "http://127.0.0.1:8000";
+  const BASE_URL = "https://plant-website-backend-3y8q.onrender.com";
 
   // ✅ INSTANT DATA FROM PREVIOUS PAGE
   const [blog, setBlog] = useState(location.state || null);
