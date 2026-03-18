1a8b71dc68f23b298ea0024619d3d33f591e7405
MonishaJ1997 <monishapython@vtscoders.com>
2026-03-18 10:48:10 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ import { FaSeedling, FaHandHoldingHeart, FaTint, FaSpa } from "react-icons/fa";
 import Footer from "./Footer";
 import plantImg from "../assets/plantlanding.png";
 function Home(){
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://plant-website-backend-3y8q.onrender.com";
 const [image,setImage] = useState("");
 const navigate = useNavigate();
 useEffect(()=>{
